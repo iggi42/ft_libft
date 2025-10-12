dc2ae6a99a8635471a146b49baa00a539db916ab
Ignatz <iggy@m3lt.work>
2025-10-13 00:07:06 +0200

    split up ft_lib_ctype into ft_{function}.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -11,41 +11,6 @@
 /* ************************************************************************** */
 #include "./libft.h"
 
-// TODO test if EOF return false for all these functions
-
-int		ft_isdigit(int c)
-{
-	return ('0' <= c && c <= '9');
-}
-
-int		ft_isascii(int c)
-{
-	return (0 <= c && c <= 127);
-}
-
-int		ft_isprint(int c)
-{
-	return (' ' <= c && c <= '~');
-}
-
-int		ft_isalpha(int c)
-{
-	return (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'));
-}
-
-int		ft_isalnum(int c)
-{
-	return (ft_isalpha(c) || ft_isdigit(c));
-}
-
-int		ft_toupper(int c)
-{
-	if ('a' <= c && c <= 'z')
-		return (c - 32);
-	else
-		return (c);
-}
-
 int		ft_tolower(int c)
 {
 	if ('A' <= c && c <= 'Z')
