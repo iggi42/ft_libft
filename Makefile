8ccba53ced1bd0059a5d585b68339a5838de164f
Felix Krüger <fkruger@students.42vienna.com>
2025-10-25 13:58:29 +0200

    make memmove behave

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: fkruger <fkruger@student.42vienna.com>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/09/30 16:10:11 by fkruger           #+#    #+#              #
-#    Updated: 2025/09/30 20:38:16 by fkruger          ###   ########.fr        #
+#    Updated: 2025/10/25 13:28:27 by fkruger          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,7 +17,7 @@ C_FILES = ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdig
 			 ft_isprint.c ft_memcmp.c ft_memcpy.c ft_memset.c ft_memchr.c ft_strchr.c \
 			 ft_strdup.c ft_strlcat.c ft_strlcpy.c ft_strlen.c ft_strncmp.c ft_strrchr.c \
 			 ft_tolower.c ft_toupper.c ft_atoi.c ft_strnstr.c ft_substr.c ft_strjoin.c \
-			 ft_strtrim.c
+			 ft_strtrim.c ft_memmove.c
 
 HEADER = $(NAME).h
 LIB = $(NAME).a
---
+++ ft_memmove.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkruger <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 19:17:42 by fkruger           #+#    #+#             */
+/*   Updated: 2025/10/25 13:55:26 by fkruger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void static *ft_memcpy_r(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = n;
+	while (i > 0)
+	{
+		*((t_byte *) dest + (i - 1)) = *((t_byte *) src + (i - 1));
+		i--;
+	}
+
+	return (dest);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	if (dest < src)
+		return (ft_memcpy(dest, src, n));
+	else
+	if (dest > src)
+		return (ft_memcpy_r(dest, src, n));
+	else
+		return (dest);
+}
