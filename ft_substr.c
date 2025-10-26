d5e92de786a75aef6f6c902e7e3601fa598033b8
Felix Krüger <fkruger@students.42vienna.com>
2025-10-26 13:55:28 +0100

    one last substr fix

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fkruger <fkruger@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 08:31:03 by fkruger           #+#    #+#             */
-/*   Updated: 2025/10/26 13:37:41 by fkruger          ###   ########.fr       */
+/*   Updated: 2025/10/26 13:55:09 by fkruger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,10 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	if (output_len <= 0)
 		return (ft_calloc(1, sizeof(char)));
 	result = ft_calloc(output_len + 1, sizeof(char));
-	while (*(s + start + i) && (i + start) <= len)
+	while (*(s + start + i) && i < len)
 	{
 		*(result + i) = *(s + start + i);
 		i++;
 	}
 	return (result);
 }
-
-/*
-#include <stdio.h>
-int main(void)
-{
-	char *r = ft_substr("kraut", 1, 1);
-	printf("result: \"%s\"\n", r);
-	return (0);
-}*/
