13b5ecc894e96d7671fc3db1253c605f29a6910b
Felix Krüger <fkruger@students.42vienna.com>
2025-10-26 13:38:26 +0100

    redo substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,19 +6,36 @@
 /*   By: fkruger <fkruger@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 08:31:03 by fkruger           #+#    #+#             */
-/*   Updated: 2025/10/17 08:31:22 by fkruger          ###   ########.fr       */
+/*   Updated: 2025/10/26 13:37:41 by fkruger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// this should return NULL, not an empty string on start (ft_strlen(s) < start)
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*result;
+	size_t	i;
+	long	output_len;
 
-	if (ft_strlen(s) < start)
-		return ("");
-	result = ft_calloc(len + 1, sizeof(char));
-	return (ft_memcpy(result, s + start, len));
+	i = 0;
+	output_len = ft_strlen(s) - start;
+	if (output_len <= 0)
+		return (ft_calloc(1, sizeof(char)));
+	result = ft_calloc(output_len + 1, sizeof(char));
+	while (*(s + start + i) && (i + start) <= len)
+	{
+		*(result + i) = *(s + start + i);
+		i++;
+	}
+	return (result);
 }
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	char *r = ft_substr("kraut", 1, 1);
+	printf("result: \"%s\"\n", r);
+	return (0);
+}*/
