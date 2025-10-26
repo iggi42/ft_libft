f879831d85587b1ab4b68ac524806bbd7fc26727
Felix Krüger <fkruger@students.42vienna.com>
2025-10-26 14:59:40 +0100

    get substr to all green

--- ft_substr.c
+++ ft_substr.c
@@ -6,21 +6,34 @@
 /*   By: fkruger <fkruger@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 08:31:03 by fkruger           #+#    #+#             */
-/*   Updated: 2025/10/26 13:55:09 by fkruger          ###   ########.fr       */
+/*   Updated: 2025/10/26 14:58:14 by fkruger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// returns the size of the substring
+static size_t	ft_substr_len(const char *s, unsigned int start, size_t len)
+{
+	size_t	s_len;
+
+	s_len = ft_strlen(s);
+	if (start > s_len)
+		return (0);
+	if ((start + len) > s_len)
+		return (s_len - start);
+	return (len);
+}
+
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*result;
 	size_t	i;
-	long	output_len;
+	size_t	output_len;
 
 	i = 0;
-	output_len = ft_strlen(s) - start;
-	if (output_len <= 0)
+	output_len = ft_substr_len(s, start, len);
+	if (output_len == 0)
 		return (ft_calloc(1, sizeof(char)));
 	result = ft_calloc(output_len + 1, sizeof(char));
 	while (*(s + start + i) && i < len)
