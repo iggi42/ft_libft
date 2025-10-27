d1c0e7b749145f04e09dbde57e37fde2cd7fb696
Ignatz <iggy@m3lt.work>
2025-10-27 02:38:00 +0100

    do ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,29 @@
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+  char *result;
+  size_t s_len;
+  size_t i;
+
+  i = 0;
+  s_len = ft_strlen(s);
+  result = ft_calloc(s_len + 1, sizeof(char));
+  while (i <= s_len)
+  {
+    result[i] = f(i, s[i]);
+    i++;
+  }
+  return (result);
+}
+
+/* char addOne(unsigned int i, char c) { return (i + c); }
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("joa,: \"%s\"\n", ft_strmapi("01234", addOne));
+  return (0);
+} */
