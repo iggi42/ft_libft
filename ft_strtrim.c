b6798a1d36746c1c9d2c6d632f69b36ca7f33e9c
Felix Krüger <fkruger@students.42vienna.com>
2025-10-28 23:04:11 +0100

    remove unused header

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,11 @@
 /*   By: fkruger <fkruger@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 08:28:48 by fkruger           #+#    #+#             */
-/*   Updated: 2025/10/28 17:17:56 by fkruger          ###   ########.fr       */
+/*   Updated: 2025/10/28 22:24:21 by fkruger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-#include <stdio.h>
 
 char	*ft_strtrim(char const *src, char const *set)
 {
