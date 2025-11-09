e1eea71ed5ec0a61c8a70a32d29b98072b26ede8
Felix Krüger <fkruger@students.42vienna.com>
2025-11-09 18:22:55 +0100

    fix norm on lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fkruger <fkruger@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 04:56:24 by fkruger           #+#    #+#             */
-/*   Updated: 2025/10/29 04:56:26 by fkruger          ###   ########.fr       */
+/*   Updated: 2025/11/09 17:43:19 by fkruger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-  t_list *result;
-  void *new_content;
+	t_list	*result;
+	void	*new_content;
 
-  if (lst == NULL)
-    return (NULL);
-  new_content = f(lst->content);
-  result = ft_lstmap(lst->next, f, del);
-  ft_lstadd_front(&result, ft_lstnew(new_content));
-  return (result);
+	if (lst == NULL)
+		return (NULL);
+	new_content = f(lst->content);
+	result = ft_lstmap(lst->next, f, del);
+	ft_lstadd_front(&result, ft_lstnew(new_content));
+	return (result);
 }
