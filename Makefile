8adad9cd50c2ab4da23024d922eeb4f8f68208ff
Ignatz <iggi@m3lt.work>
2025-11-14 21:12:02 +0100

    [WIP] printf: parse template done

--- Makefile
+++ Makefile
@@ -21,7 +21,7 @@ C_FILES = ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdig
 			ft_strtrim.c ft_memmove.c ft_split.c ft_strmapi.c ft_striteri.c ft_putstr_fd.c \
 			ft_putendl_fd.c ft_putchar_fd.c ft_itoa.c ft_putnbr_fd.c ft_lstnew.c ft_lstadd_front.c \
 			ft_lstsize.c ft_lstlast.c ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c \
-			ft_lstmap.c ft_lstfold.c
+			ft_lstmap.c ft_lstfold.c ft_printf.c
 
 HEADER = $(NAME).h
 LIB = $(NAME).a
--- ft_printf.c
+++ ft_printf.c
@@ -3,64 +3,125 @@
 
 typedef struct s_iol_el
 {
-	t_byte		*buffer;
+	char *buffer;
 	size_t		size;
 }				t_iol_el;
 
-typedef struct s_iol_fld_acc
+static size_t get_seg_size(char *template)
 {
-	char		*runner;
-	t_list		**io_list;
-}				t_ifa;
+	char *seeker;
 
-// return rest of the template
-// @param op_start points to where an operator '%' is.
-static char	*add_op(t_list **io_list, char *op_start)
-{
-	t_list	*new_lnk;
-
-	new_lnk = ft_lstnew(op_start);
-	ft_lstadd_back(io_list, new_lnk);
-	return (op_start + 2);
+  if (*template == '\0')
+  	return 0;
+	if (*template == '%')
+		return 2;
+	seeker = ft_strchr(template, '%');
+	if (seeker == NULL)
+		return ft_strlen(template);
+	else
+		return (seeker - template);
 }
 
-static t_ifa	*ft_ifa(const char *tmpl)
+static t_list	*parse_template(char *template)
 {
-	t_ifa	*result;
-
-	result = (t_ifa *)malloc(sizeof(t_ifa));
-	result->runner = (char *)tmpl;
-	result->io_list = ft_lststart(NULL);
-	return (result);
-}
-
-static t_iol_el	*ft_iol_new(t_byte *buffer, size_t size)
-{
-	t_iol_el	*result;
-
-	result = (t_iol_el *)malloc(sizeof(t_iol_el));
-	result->buffer = buffer;
-	result->size = size;
-	return (result);
-}
-
-static t_list	*fold_template(char *template)
-{
+	t_list *result;
+  t_iol_el *seg;
+  
 	if (template == NULL || *template == '\0')
 		return (NULL);
-	return (io_list);
+
+	seg = (t_iol_el *)malloc(sizeof(t_iol_el));
+	seg->buffer = template;
+	seg->size = get_seg_size(template);
+	result = ft_lstnew(seg);
+	result->next = parse_template(template + seg->size);
+
+	return (result);
+}
+
+#include <unistd.h>
+static int ft_iol_write(t_list *l, int fd)
+{
+	t_iol_el *el;
+	int r;
+	int rr;
+
+	if(l == NULL)
+		return -1;
+	el = (t_iol_el *) l->content;
+	r = write(fd, el->buffer, el->size);
+	if (r == -1)
+		return -1;
+	rr = ft_iol_write(l->next, fd);
+	if (rr == -1)
+		return -1;
+	return (r + rr);
+}
+
+// DEBUG function
+#include <stdio.h>
+static void ft_iol_pp_el(t_iol_el *el)
+{
+	if (el == NULL)
+		printf("[NULL] io element");
+	else
+		printf("%ld : %s\n", el->size, el->buffer);
+}
+
+static void ft_iol_pp(t_list *l)
+{
+	t_iol_el *el;
+	int r;
+
+	if (l == NULL)
+	{
+		write(1, "[NULL]\n", 7);
+	  return;
+	}
+  ft_iol_pp_el((t_iol_el *) l->content);
+	ft_iol_pp(l->next);
+}
+
+static void escape_el(t_iol_el *el)
+{
+	
+}
+
+static void make_str(t_iol_el *el, char* str)
+{
+	
+}
+
+static void make_char(t_iol_el *el, char c)
+{
+	
+}
+
+void replace_ops(t_list *l)
+{
+	
 }
 
 int	ft_printf(const char *template, ...)
 {
 	va_list	args;
-	t_list	**io_list;
+	t_list	*seg_list;
+	t_list *lel;
+	t_iol_el *io_el;
 
-	io_list = fold_template((char *)template);
-	va_start(args, template);
-	while (found != NULL)
-	{
-		found = add_op(io_list, found);
-	}
-	return (size);
+	seg_list = parse_template((char *)template);
+	lel = seg_list;
+  va_start(args, template);
+  replace_ops(seg_list, args);
+	va_end(args);
+	//ft_iol_pp(seg_list);
+	return ft_iol_write(seg_list, 1);
+}
+
+int main(void)
+{
+	ft_printf("joasj%sjoa\n");
+	ft_printf("[[%s]]\n");
+	ft_printf("pe%dnis\n");
+	ft_printf("pe%pnis\n");
 }
