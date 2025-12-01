689d1d177aa3691f5fb9b644667272cd2b78020b
Ignatz <iggy@m3lt.work>
2025-12-01 17:51:14 +0100

    add ft_buf from to libft.h

--- libft.h
+++ libft.h
@@ -231,6 +231,51 @@ int ft_iol_write(t_list *l, int fd);
  */
 void ft_iol_del(t_list **l);
 
-
 //!@}
+
+//! @name buffer list, basic get_next_line is written on this
+//!@{
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+/** @brief wrapper of a buffer, together with its size; */
+typedef struct s_buf
+{
+	size_t				size;
+	t_byte				*p;
+}						t_buf;
+
+
+/** @brief create a new buffer with a given size and allocate a buffer for it. */
+t_buf					*ft_buf_new(size_t size);
+
+/** @brief create a max BUFFER_SIZE buffer from a file descriptior fd. */
+t_buf					*ft_buf_read(int fd);
+
+/**
+ @brief to split buffer at a given pointer in it into 2 buffers.
+ the return value is the part to the left,
+ and the given pointer will be the last position of the it.
+ and the buffer 'b' will the one on the left of the index.
+ */
+t_buf					*ft_buf_split(t_buf **b, size_t left_len);
+
+/** @brief concat to buffers together and frees the inputs */
+t_buf					*ft_buf_cat(t_buf **left, t_buf **right);
+
+/** @brief frees the memory of the given pointer and sets the address of it to NULL */
+void					ft_buf_free(t_buf **b);
+
+/** @brief turn a buffer struct into a zero index c string */
+char					*ft_buf_str(t_buf **b);
+
+/** @brief copy a buffer from src to dest, basically cloning it */
+void					ft_buf_cp(t_buf *src, t_buf *dest);
+
+/** @brief find the first occurence of any bytes in args (null terminated string) */
+t_byte					*ft_buf_chr(t_buf *b, t_byte *args);
+//!@}
+
 #endif
