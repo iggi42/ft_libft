caf29b3834283f7cfcffe3719e883efbc4ac4ac0
Ignatz <iggy@m3lt.work>
2025-12-06 14:08:23 +0100

    make ft_itoa use the ultoa_bl backend

--- ft_itoa.c
+++ ft_itoa.c
@@ -11,50 +11,7 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-static char	*base_str(long nb)
+char	*ft_itoa(int i)
 {
-	size_t	size;
-
-	size = 2;
-	if (nb < 0)
-	{
-		nb *= -1;
-		size++;
-	}
-	while (nb / 10 > 0)
-	{
-		nb /= 10;
-		size++;
-	}
-	return (ft_calloc(size, sizeof(char)));
-}
-
-static size_t	write2str(long nb, char *dst)
-{
-	size_t	size;
-
-	if (nb == 0)
-		return (0);
-	size = write2str(nb / 10, dst);
-	dst[size] = (nb % 10) + '0';
-	return (size + 1);
-}
-
-char	*ft_itoa(int nb)
-{
-	char	*result;
-
-	if (nb == 0)
-		return (ft_strdup("0"));
-	result = base_str(nb);
-	if (result == NULL)
-		return (NULL);
-	if (nb < 0)
-	{
-		result[0] = '-';
-		write2str(((long)nb) * -1, result + 1);
-	}
-	else
-		write2str(nb, result);
-	return (result);
+	return (ft_itoa_b(i, FT_ALPHABET_DEC));
 }
